761002cea2db1362327982c91c385d247e99f74e
Christopher868 <Christopherhicks868@gmail.com>
2025-11-01 17:29:01 -0400

    Created navbar

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector('#menu-btn');
+const collapsingMenu = document.querySelector('#collapsing-menu')
+
+// Click event for toggle dropdown menu
+document.addEventListener('click', (e) => {
+    if(e.target.closest('svg') === menuBtn) {
+        collapsingMenu.classList.toggle('hidden');
+    } else if(e.target.closest('svg') !== menuBtn && e.target.closest('div') !== collapsingMenu){
+        collapsingMenu.classList.add('hidden');
+    }
+    console.log(e.target)
+});
